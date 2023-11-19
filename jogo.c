5b9029431ace77cf90ca7b2575d9100068c1b158
Maria Clara G <mariaclagouv@gmail.com>
2023-11-19 12:58:18 -0300

    primeiro commit

---
+++ jogo.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void desenharForca () 
+{
+    //array da forca(com o indice 0 sendo o desenho da forca)
+}
+
+int main() {
+
+    const int TENTATIVAS = 6;
+
+    char palavra[250], letras_erradas[27], letra, status_atual;
+    int  qtd_erro = 0, qtd_acerto = 0, jogDaVez, qtd_jogadores, posicao, i;
+
+    //Quantos jogadores sao
+
+    //receber palavra chave
+
+    //do
+        //if(qtd_jogadores > 1) 
+            //se jogDaVez % 2 = 0
+                //print hora do jogador 2
+            //else
+                //print hora do jogador 1
+    
+        //aparecer forca e a quantidade de espaço de letras
+
+        //pedir a letra da vez
+
+        //for para comparar a letra com as letras da palavra
+            //se tiver uma letra igual a alguma letra da palavra 
+                //qtd de acerto ++
+                //
+            //senao, 
+                //qtd_erro++
+                //desenhar forca com um membro a mais
+
+    //while (o jogo continuar)
+
+    //se ganhar o jogo 
+        //printar mensagem de vencedor
+    //senao
+        //vc perdeu, lamento
+
+
+
+}
